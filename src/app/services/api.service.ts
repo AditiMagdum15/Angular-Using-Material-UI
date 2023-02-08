aa88f63e7317c617042ddf09cf9fc1bee2423eb4
AditiMagdum15 <aditimagdum15@gmail.com>
2023-02-08 19:17:26 +0530

    second

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,19 +9,19 @@ export class ApiService {
   constructor(private http : HttpClient) {}
 
     postProduct(data : any){
-      return this.http.post<any>("http://localhost:3000/productList/", data);
+      return this.http.post<any>("https://my-json-server.typicode.com/AditiMagdum15/Angular-Using-Material-UI/productList/", data);
     }
 
     getProduct(){
-      return this.http.get<any>("http://localhost:3000/productList/");
+      return this.http.get<any>("https://my-json-server.typicode.com/AditiMagdum15/Angular-Using-Material-UI/productList/");
     }
 
     putProduct(data: any, id: number){
-      return this.http.put<any>("http://localhost:3000/productList/"+ id,data);
+      return this.http.put<any>("https://my-json-server.typicode.com/AditiMagdum15/Angular-Using-Material-UI/productList/"+ id,data);
     }
 
     deleteProduct(id: number){
-      return this.http.delete<any>("http://localhost:3000/productList/"+id);
+      return this.http.delete<any>("https://my-json-server.typicode.com/AditiMagdum15/Angular-Using-Material-UI/productList/"+id);
     }
 
 }
